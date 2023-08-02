36782178111a6609224af50c56cde61952be6a82
Ronny Herrera <rherrera@sintad.pe>
2023-08-01 20:19:52 -0500

    Input-Text base

--- projects/project-components-rdh/src/lib/input-text/input-text.component.ts
+++ projects/project-components-rdh/src/lib/input-text/input-text.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-input-text',
+  selector: 'input-text',
   templateUrl: './input-text.component.html',
-  styleUrls: ['./input-text.component.css']
+  styleUrls: ['./input-text.component.css'],
 })
 export class InputTextComponent implements OnInit {
+  @Input() id: string = '';
+  @Input() placeholder: string = '';
+  @Input() label: string = '';
+  @Input() format: string | ['mm', 'MM', 'Mm', ''] = '';
+  @Input() value: string = '';
+  @Input() disabled: boolean = false;
+  @Input() readonly: boolean = false;
+  @Input() readonlyPlainText: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  transformClass(): string[] {
+    const classList: string[] = [];
+    // Default
+    classList.push(this.format as string);
+    classList.push(this._setClassForFormControl());
+    return classList;
   }
 
+  private _setClassForFormat(): string {
+    switch (this.format) {
+      case 'mm':
+        return 'minuscula';
+      case 'MM':
+        return 'mayuscula';
+      case 'Mm':
+        return 'capitalizable';
+      default:
+        return '';
+    }
+  }
+
+  private _setClassForFormControl(): string {
+    if (this.readonlyPlainText) {
+      this.readonly = true;
+      return 'form-control-plaintext';
+    }
+    return 'form-control';
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
